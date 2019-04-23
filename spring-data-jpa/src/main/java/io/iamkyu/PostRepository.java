cb1acc3ab4cfd17fc4230259646c8ebac1ce176d
iamkyu <kyujnam@gmail.com>
2019-04-24 08:52:38 +0900

    Named Parameter & SpEL

--- spring-data-jpa/src/main/java/io/iamkyu/PostRepository.java
+++ spring-data-jpa/src/main/java/io/iamkyu/PostRepository.java
@@ -13,4 +13,8 @@ public interface PostRepository extends JpaRepository<Post, Long> {
 
     @Query("select p from Post AS p where p.title = ?1")
     List<Post> myFindByTitleSortByTitleLength(String title, Sort sort);
+
+    // Named Parameter 와 SpEL 사용 예
+    @Query("select p from #{#entityName} AS p where p.title = :title")
+    List<Post> myFindByTitleSortByTitleLength2(String title, Sort sort);
 }
